d46287cebd503b8e3ab429afbf455318e1e1c85b
FroyouGitt <barca_ybf@hotmail.com>
2017-04-04 19:49:51 +0200

    kan scanne fra samsung

--- src/pages/items-add-tag-scann-admin/items-add-tag-scann-admin.ts
+++ src/pages/items-add-tag-scann-admin/items-add-tag-scann-admin.ts
@@ -1,30 +1,39 @@
-import { Component, NgZone } from '@angular/core';
+﻿import { Component, NgZone } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 import { ItemsAddSuccessAdminPage } from '../items-add-success-admin/items-add-success-admin';
+import { TagUtil, Tag } from '../../classes/tag';
 
 @Component({
   selector: 'page-items-add-tag-scann-admin',
   templateUrl: 'items-add-tag-scann-admin.html'
 })
 export class ItemsAddTagScannAdminPage {
+   dataReceived: boolean;
   close = false;
   itemName = "";
+  tag: Tag;
 
   constructor(public navCtrl: NavController,
   public navParams: NavParams,
   public zone: NgZone,
   public platform: Platform) {
-    this.itemName = navParams.get("itemName");
-    if((<any>window).nfc != null) {
-      (<any>window).nfc.addNdefListener(this.onTagFound.bind(this));
-      (<any>window).nfc.addTagDiscoveredListener(this.onTagFound.bind(this));
-    }
+
+      this.tag = new Tag();
+
+      console.log("kommer til scanklassen");
+      this.itemName = navParams.get("itemName");
+
+    
+      (<any>window).nfc.addNdefListener((tagEvent: Event) => this.tagListenerSuccess(tagEvent));
+      (<any>window).nfc.addTagDiscoveredListener((tagEvent: Event) => this.tagListenerSuccess(tagEvent));
+    
 
     platform.registerBackButtonAction(this.onBackPressed.bind(this));
   }
 
-  onTagFound(nfcEvent) {
+  /*onTagFound(nfcEvent) {
+      console.log("tag funnet");
     if(!this.close) {
       this.zone.run(() => {
       var tagId = (<any>window).nfc.bytesToHexString(nfcEvent.tag.id);
@@ -35,8 +44,21 @@ export class ItemsAddTagScannAdminPage {
     });
     this.close = true;
     }
-  }
+  }*/
 
+  tagListenerSuccess(tagEvent: Event) {
+      console.log(tagEvent);
+      this.zone.run(() => {
+          this.tag = TagUtil.readTagFromJson(tagEvent);
+          var tagId = (<any>window).nfc.bytesToHexString(this.tag.id);
+          this.dataReceived = true;
+          this.navCtrl.push(ItemsAddSuccessAdminPage, {
+              itemName: this.itemName,
+              tagId: tagId
+          });
+      });
+
+  }
   onBackPressed() {
     this.close = true;
     this.navCtrl.pop();
