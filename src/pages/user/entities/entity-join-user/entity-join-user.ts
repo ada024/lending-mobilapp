64090390a4c880729af3fa9db16d1c5a9d644d85
Daniel Losvik <95danlos65@gmail.com>
2017-05-16 12:57:23 +0200

    fix

--- src/pages/user/entities/entity-join-user/entity-join-user.ts
+++ src/pages/user/entities/entity-join-user/entity-join-user.ts
@@ -13,12 +13,22 @@ export class EntityJoinUserPage {
   hasJoined = false;
   firstAnswerLoaded = false;
   bothAnswersLoaded = false;
+  
+  currentEntityLocation;
+  currentEntityRoom;
+  currentEntityHours;
+  currentEntityDays;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
    public zone: NgZone, public db: DatabaseService) {
     this.entity = navParams.get("entity");
     db.isPending(this.entity, this.onAnswerLoaded1.bind(this));
     db.hasJoined(this.entity, this.onAnswerLoaded2.bind(this));
+
+    this.currentEntityLocation = this.entity.office.location;
+    this.currentEntityRoom = this.entity.office.room;
+    this.currentEntityHours = this.entity.office.hours;
+    this.currentEntityDays = this.getWeekDays(this.entity.office.days.length);
   }
 
   onAnswerLoaded1(answer) {
@@ -43,5 +53,43 @@ export class EntityJoinUserPage {
 
   sendRequest() {
     this.db.addPendingUser(this.entity);
+  }
+
+  getWeekDays(n) {
+      var weekday = new Array(7);
+      weekday[0] = "Sundays";
+      weekday[1] = "Mondays";
+      weekday[2] = "Tuesdays";
+      weekday[3] = "Wednesdays";
+      weekday[4] = "Thursdays";
+      weekday[5] = "Fridays";
+      weekday[6] = "Saturdays";
+
+      var days = this.entity.office.days;
+
+      var dayInfo = null;
+      if (n == 1) {
+          dayInfo = weekday[days[0]];
+      }
+      if (n == 2) {
+          dayInfo = weekday[days[0]] + " and " +  weekday[days[1]];
+      }
+      if (n == 3) {
+          dayInfo = weekday[days[0]] + ", " + weekday[days[1]] + " and " + weekday[days[2]];
+      }
+      if (n == 4) {
+          dayInfo = weekday[days[0]] + ", " + weekday[days[1]] + ", " + weekday[days[2]] + " and " + weekday[days[3]];
+      }
+      if (n == 5) {
+          dayInfo = weekday[days[0]] + ", " + weekday[days[1]] + ", " + weekday[days[2]] + ", " + weekday[days[3]] + " and " + weekday[days[4]];
+      }
+      if (n == 6) {
+          dayInfo = weekday[days[0]] + ", " + weekday[days[1]] + ", " + weekday[days[2]] + ", " + weekday[days[3]] + ", " + weekday[days[4]] + " and " + weekday[days[5]];
+      }
+      if (n == 7) {
+          dayInfo = "Every day"
+      }
+
+      return dayInfo;
   }
 }
