8404892b6bcd7f49032c95ae5d7d48be0a7fed03
Daniel Losvik <95danlos65@gmail.com>
2017-06-01 17:49:54 +0200

    fix

---
+++ src/pages/admin/users/users-tabs/users-tabs.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-users-tabs',
+  templateUrl: 'users-tabs.html'
+})
+export class UsersTabsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
