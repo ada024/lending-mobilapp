0c7551a2dffab516c9b31fd7fe18f2181a7be9f0
Daniel Losvik <95danlos65@gmail.com>
2017-07-13 20:26:15 +0200

    hei

--- src/pages/admin/users/users-details-admin/users-details-admin.ts
+++ src/pages/admin/users/users-details-admin/users-details-admin.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { DatabaseService } from '../../../../providers/database-service';
 import { UsersAddTagAdminPage } from '../users-add-tag-admin/users-add-tag-admin';
@@ -9,10 +9,19 @@ import { SendMailPage } from '../../../send-mail/send-mail';
   templateUrl: 'users-details-admin.html'
 })
 export class UsersDetailsAdminPage {
+  currentEntity;
   user;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public db: DatabaseService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, 
+    public zone: NgZone, public db: DatabaseService) {
     this.user = navParams.get("user");
+    db.getEntity(this.onEntityLoaded.bind(this));
+  }
+
+  onEntityLoaded(currentEntity) {
+    this.zone.run(() => {
+      this.currentEntity = currentEntity[0];
+    });
   }
 
   goToSendMailPage() {
